fd74e7e288a4b1f5f8ed1d25dfe7ea80f71d4cca
Jérémy Lal <kapouer@melix.org>
2016-06-07 15:48:50 +0200

    Exclude, prepend, and serialize options

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ var URL = require('url');
 
 module.exports = bundledom;
 
-function bundledom(path) {
+function bundledom(path, opts) {
 	var output;
 	return loadDom(path).then(function(doc) {
-		return processScripts(doc).then(function(js) {
+		return processScripts(doc, opts).then(function(js) {
 			output = js;
 			return doc;
 		});
 	}).then(function(doc) {
-		return processStylesheets(doc).then(function(css) {
+		return processStylesheets(doc, opts).then(function(css) {
 			output += '\n(' + function() {
 				var sheet = document.createElement('style');
 				sheet.type = 'text/css';
@@ -30,21 +30,25 @@ function bundledom(path) {
 			return doc;
 		});
 	}).then(function(doc) {
-		return processImports(doc).then(function(imports) {
+		return processImports(doc, opts).then(function(imports) {
 			output += imports;
 			return doc;
 		});
-	}).then(function() {
+	}).then(function(doc) {
+		if (opts.serialize) fs.writeFileSync(opts.serialize, doc.documentElement.outerHTML);
 		return output;
 	});
 }
 
-function processImports(doc) {
+function processImports(doc, opts) {
 	var path = URL.parse(doc.baseURI).pathname;
 	var docRoot = Path.dirname(path);
 	var astRoot = [];
 	return Promise.all(doc.queryAll('head > link[href][rel="import"]').map(function(node) {
-		var src = Path.join(docRoot, node.getAttribute('href'));
+		var src = node.getAttribute('href');
+		if (exclude(src, opts.exclude)) return;
+		removeNodeSpace(node);
+		src = Path.join(docRoot, src);
 		return loadDom(src).then(function(idoc) {
 			// limited Link rel=import support: only inline <script> and <style> are supported
 			var textStyle = idoc.queryAll('style').map(function(node) {
@@ -107,11 +111,16 @@ function processImports(doc) {
 	});
 }
 
-function processScripts(doc) {
+function processScripts(doc, opts) {
 	var docRoot = Path.dirname(URL.parse(doc.baseURI).pathname);
 	var astRoot;
+	prependList(doc.query('head > script[src]'), opts.prepend, 'script', 'src', 'js');
+
 	return Promise.all(doc.queryAll('head > script[src]').map(function(node) {
-		var src = Path.join(docRoot, node.getAttribute('src'));
+		var src = node.getAttribute('src');
+		if (exclude(src, opts.exclude)) return;
+		removeNodeSpace(node);
+		src = Path.join(docRoot, src);
 		return readFile(src).then(function(data) {
 			var ast = uglify.parse(data.toString(), {filename: src, toplevel: astRoot});
 			if (!astRoot) astRoot = ast;
@@ -126,12 +135,15 @@ function processScripts(doc) {
 	});
 }
 
-function processStylesheets(doc) {
+function processStylesheets(doc, opts) {
 	var path = URL.parse(doc.baseURI).pathname;
 	var docRoot = Path.dirname(path);
 	var astRoot;
 	return Promise.all(doc.queryAll('head > link[href][rel="stylesheet"]').map(function(node) {
-		var src = Path.join(docRoot, node.getAttribute('href'));
+		var src = node.getAttribute('href');
+		if (exclude(src, opts.exclude)) return;
+		removeNodeSpace(node);
+		src = Path.join(docRoot, src);
 		return readFile(src).then(function(data) {
 			var ast = postcss.parse(data.toString(), {
 				from: src,
@@ -152,6 +164,56 @@ function processStylesheets(doc) {
 	});
 }
 
+function exclude(src, list) {
+	if (!list) return;
+	var found = list.some(function(str) {
+		return ~src.indexOf(str);
+	});
+	if (found) console.warn('Excluded:', src);
+	return found;
+}
+
+function filterByExt(list, ext) {
+	if (!list) return [];
+	ext = '.' + ext;
+	return list.filter(function(src) {
+		return Path.extname(URL.parse(src).pathname) == ext;
+	});
+}
+
+function removeNodeSpace(node) {
+	var cur = node.previousSibling;
+	while (cur && cur.nodeType == 3) {
+		cur.remove();
+		cur = node.previousSibling;
+	}
+	node.remove();
+}
+
+function spaceBefore(node) {
+	var str = "";
+	var cur = node.previousSibling;
+	while (cur && cur.nodeType == 3) {
+		str = cur.nodeValue + str;
+		cur = cur.previousSibling;
+	}
+	return node.ownerDocument.createTextNode(str);
+}
+
+function prependList(pivot, list, tag, att, ext) {
+	if (!pivot) {
+		console.error("Cannot prepend before no node", tag, att, ext);
+		return;
+	}
+	var textNode = spaceBefore(pivot);
+	filterByExt(list, ext).forEach(function(src) {
+		var node = pivot.ownerDocument.createElement(tag);
+		node[att] = src;
+		pivot.before(node);
+		pivot.before(textNode.cloneNode());
+	});
+}
+
 function loadDom(path) {
 	return readFile(path).then(function(data) {
 		var doc = jsdom(data, {
@@ -167,6 +229,13 @@ function loadDom(path) {
 		};
 		doc.queryAll = function(sel) {
 			return Array.from(doc.querySelectorAll(sel));
+		};
+		win.Node.prototype.before = function(node) {
+			this.parentNode.insertBefore(node, this);
+		};
+		win.Node.prototype.after = function(node) {
+			if (this.nextSibling) this.parentNode.insertBefore(node, this.nextSibling);
+			else this.parentNode.appendChild(node);
 		};
 		return doc;
 	});
