5bad1c0b4b090c7abd206bca4a9f93c277c42d97
Jérémy Lal <kapouer@melix.org>
2016-12-13 21:54:41 +0100

    Recursive import but no script in first import

--- test/suite.js
+++ test/suite.js
@@ -4,6 +4,26 @@ var Path = require('path');
 
 var bundledom = require('..');
 
+function runDom(htmlPath, data) {
+	return new Promise(function(resolve, reject) {
+		jsdom.jsdom(data.html, {
+			virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+			url: 'file://' + Path.resolve(htmlPath),
+			features: {
+				FetchExternalResources: ['script'],
+				ProcessExternalResources: ['script']
+			},
+			created: function(err, win) {
+				if (err) return reject(err);
+				win.onload = function() {
+					win.eval(data.js);
+					resolve(win.document);
+				};
+			}
+		});
+	});
+}
+
 it('should do the most simplest basic test', function() {
 	return bundledom('test/fixtures/basic.html', {
 		exclude: []
@@ -38,28 +58,26 @@ it('should ignore a script', function() {
 it('should bundle html import and run it', function() {
 	var filepath = 'test/fixtures/import.html';
 	return bundledom(filepath).then(function(data) {
-		console.log(data);
 		data.should.have.property('js');
 		data.should.have.property('css');
 		data.should.have.property('html');
-		return new Promise(function(resolve, reject) {
-			jsdom.jsdom(data.html, {
-				virtualConsole: jsdom.createVirtualConsole().sendTo(console),
-				url: 'file://' + Path.resolve(filepath),
-				features: {
-					FetchExternalResources: ['script'],
-					ProcessExternalResources: ['script']
-				},
-				created: function(err, win) {
-					if (err) return reject(err);
-					win.onload = function() {
-						win.eval(data.js);
-						should.exist(win.document.querySelector('head > style'));
-						should.exist(win.document.querySelector('body > .element'));
-						resolve();
-					};
-				}
-			});
+		return runDom(filepath, data).then(function(doc) {
+			should.exist(doc.querySelector('head > style'));
+			should.exist(doc.querySelector('body > .element'));
 		});
 	});
 });
+
+it('should bundle html import in html import and run it', function() {
+	var filepath = 'test/fixtures/import-in-import.html';
+	return bundledom(filepath).then(function(data) {
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		return runDom(filepath, data).then(function(doc) {
+			should.exist(doc.querySelector('head > style'));
+			should.exist(doc.querySelector('body > .element'));
+		});
+	});
+});
+
