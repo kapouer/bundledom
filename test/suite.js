f8ff0d1b3958d0461dde8ae5dbdd302db612264c
Jérémy Lal <kapouer@melix.org>
2016-12-13 21:27:41 +0100

    Add link[rel="import"] test and fixtures

--- test/suite.js
+++ test/suite.js
@@ -1,4 +1,6 @@
 var should = require('should');
+var jsdom = require('jsdom');
+var Path = require('path');
 
 var bundledom = require('..');
 
@@ -30,5 +32,34 @@ it('should ignore a script', function() {
 		data.should.have.property('css');
 		data.should.have.property('html');
 		data.html.indexOf('<script src="b.js"></script>').should.be.greaterThan(0);
+	});
+});
+
+it('should bundle html import and run it', function() {
+	var filepath = 'test/fixtures/import.html';
+	return bundledom(filepath).then(function(data) {
+		console.log(data);
+		data.should.have.property('js');
+		data.should.have.property('css');
+		data.should.have.property('html');
+		return new Promise(function(resolve, reject) {
+			jsdom.jsdom(data.html, {
+				virtualConsole: jsdom.createVirtualConsole().sendTo(console),
+				url: 'file://' + Path.resolve(filepath),
+				features: {
+					FetchExternalResources: ['script'],
+					ProcessExternalResources: ['script']
+				},
+				created: function(err, win) {
+					if (err) return reject(err);
+					win.onload = function() {
+						win.eval(data.js);
+						should.exist(win.document.querySelector('head > style'));
+						should.exist(win.document.querySelector('body > .element'));
+						resolve();
+					};
+				}
+			});
+		});
 	});
 });
